f9f09d22d90fe3a3651d020f81a5a0dc8b44749a
Jonas Eppelt <jonas.eppelt@gmail.com>
2022-08-04 18:03:07 +0200

    making eemumu

--- event_generation/events.py
+++ event_generation/events.py
@@ -7,9 +7,10 @@ event_dict = {
     "eepipi": ["11", "-11", "211", "-211"],
     "eek0": ["11", "-11", "310"],
     "eepi0": ["11", "-11", "111"],
-    "ee": ["11", "-11"]
+    "ee": ["11", "-11"],
+    "eemumu": ["11", "-11", "13", "-13"]
 }
 events_dict = {}
-for i in range(5):
+for i in range(100):
     for key, value in event_dict.items():
         events_dict.update({f"{key}{i}": value})
